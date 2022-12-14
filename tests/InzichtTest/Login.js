db1bf80b8c6ee3de3e31d10f992a027ce94950b7
Marcin Pogorzelski <mpogorzelski@Infoprojekt.pl>
2022-12-14 20:10:10 +0100

    folder creation

--- tests/InzichtTest/Login.js
+++ tests/InzichtTest/Login.js
@@ -1,13 +1,13 @@
 import {InzichtSettings} from '/GIT/Inzicht3_playwright/POM/InzichtSettings/';
 
 // @ts-check
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require('@playwright/test/InzichtTest');
 
 test('homepage has Playwright in title and get started link linking to the intro page', async ({ page }) => {
   await page.goto('https://04-inzicht2.imfint.local/#/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/inzicht2/);
+  await expect(page).toHaveTitle(inzicht2);
 
   // create a locator
   const getStarted = page.getByRole('link', { name: 'Get started' });
