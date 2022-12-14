b7259035783a2e1bacf81cf1a1675841e161540e
Marcin Pogorzelski <mpogorzelski@Infoprojekt.pl>
2022-12-14 20:22:26 +0100

    folder creation

--- tests/InzichtTest/Login.js
+++ tests/InzichtTest/Login.js
@@ -1,23 +1,23 @@
 import {InzichtSettings} from '../InzichtTest';
 
 // @ts-check
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require('@playwright/test/InzichtTest');
 
-test('homepage has Playwright in title and get started link linking to the intro page', async ({ page }) => {
+test('open Inzicht page', async ({ page }) => {
   await page.goto('https://04-inzicht2.imfint.local/#/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(inzicht2);
+//   // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/inzicht2/);
 
-  // create a locator
-  const getStarted = page.getByRole('link', { name: 'Get started' });
+//   // create a locator
+//   const getStarted = page.getByRole('link', { name: 'Get started' });
 
-  // Expect an attribute "to be strictly equal" to the value.
-  await expect(getStarted).toHaveAttribute('href', '/docs/intro');
+//   // Expect an attribute "to be strictly equal" to the value.
+//   await expect(getStarted).toHaveAttribute('href', '/docs/intro');
 
-  // Click the get started link.
-  await getStarted.click();
+//   // Click the get started link.
+//   await getStarted.click();
 
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
+//   // Expects the URL to contain intro.
+//   await expect(page).toHaveURL(/.*intro/);
 });
